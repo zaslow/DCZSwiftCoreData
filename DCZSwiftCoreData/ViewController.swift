7b813b230f43dc3ef08af6833808b7856916967e
Zaslow <dcz4@clg62-m119.labs.pitt.edu>
2015-04-16 14:57:52 -0400

    Initial Commit

---
+++ DCZSwiftCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DCZSwiftCoreData
+//
+//  Created by Zaslow, Dean Clayton on 4/16/15.
+//  Copyright (c) 2015 Zaslow, Dean Clayton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
