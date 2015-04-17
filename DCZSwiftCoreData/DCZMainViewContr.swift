ee5b9592ab7ce9ec5de0b33082aa1d137e90a640
Zaslow <dcz4@clg27-m17.labs.pitt.edu>
2015-04-16 20:00:41 -0400

    Core Data Application complete

--- DCZSwiftCoreData/DCZMainViewContr.swift
+++ DCZSwiftCoreData/DCZMainViewContr.swift
@@ -19,12 +19,36 @@ class DCZMainViewContr: UIViewController {
         var context:NSManagedObjectContext = appDel.managedObjectContext!
         
         var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context) as NSManagedObject
-        newUser.setValue("Test Username", forKey: "username")
-        newUser.setValue("Test Password", forKey: "password")
+        newUser.setValue("" + txtUsername.text, forKey: "username")
+        newUser.setValue("" + txtPassword.text, forKey: "password")
+        
+        context.save(nil)
+        
+        println(newUser)
+        println("Object Saved.")
     }
     
     @IBAction func btnLoad() {
+        var appDel:AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+        var context:NSManagedObjectContext = appDel.managedObjectContext!
         
+        var request = NSFetchRequest(entityName: "Users")
+        request.returnsObjectsAsFaults = false;
+        request.predicate = NSPredicate(format: "username = %@", "" + txtUsername.text)
+        
+        var results:NSArray = context.executeFetchRequest(request, error: nil)!
+        
+        if(results.count > 0) {
+            var res = results[0] as NSManagedObject
+            txtUsername.text = res.valueForKey("username") as String
+            txtPassword.text = res.valueForKey("password") as String
+            
+            // for res in results {
+            //     println(res)
+            // }
+        } else {
+                println("0 Results Found or Error")
+        }
     }
 
     override func viewDidLoad() {
